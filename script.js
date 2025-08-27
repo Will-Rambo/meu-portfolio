f7b887f2a0eacc5f2de376c306dfda4ac705fee8
Will-Rambo <willian.rambo@gmail.com>
2025-08-26 23:01:01 -0300

    correção de imagem inserção do menu hamburguer

--- script.js
+++ script.js
@@ -142,3 +142,10 @@ document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem("theme", isDark ? "dark" : "light");
   });
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
